eccaf5b9053dc3451486a274ab87d2bf29d401a3
Sebastian Schumacher <sebastian.schumacher@hotmail.de>
2020-12-01 15:20:52 +0100

    plants controller show new create methods and views

--- app/controllers/plants_controller.rb
+++ app/controllers/plants_controller.rb
@@ -3,12 +3,27 @@ class PlantsController < ApplicationController
   end
 
   def new
+    @plant = Plant.new
   end
 
   def show
+    @plant = Plant.find(params[:id])
   end
 
   def create
+    @plant = Plant.new(plant_params)
+    @plant.user = current_user
+    if @plant.save == true
+      redirect_to plant_path(@plant)
+    else
+      render :new # jumps to view "new"
+    end
+  end
+
+  private
+
+  def plant_params
+    params.require(:plant).permit(:description, :name, :price, :address)
   end
 
 end

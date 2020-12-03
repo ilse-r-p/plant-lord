c12b6db2e10a16c51473a9041825b5600eb9256e
Sebastian Schumacher <sebastian.schumacher@hotmail.de>
2020-12-03 18:07:06 +0100

    basic search working

--- app/models/plant.rb
+++ app/models/plant.rb
@@ -10,4 +10,11 @@ class Plant < ApplicationRecord
   validates :description, presence: true
   validates :address, presence: true
   validates :price, presence: true
+
+  include PgSearch::Model
+  pg_search_scope :search_by_name,
+    against: [ :name],
+    using: {
+      tsearch: { prefix: true } # <-- now `superman batm` will return something!
+    }
 end
